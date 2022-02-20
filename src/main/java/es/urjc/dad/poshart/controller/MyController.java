d5e89e69e6a3f2887e5210266894e4474693433f
evitafumar <evapastorabanades@gmail.com>
2022-02-20 17:18:31 +0100

    Header and Footer
    Se añaden el header y footer en los html
    
    Estos contienen la base de navegación de la interfaz, así como un comienzo en la definición del estilo de la aplicación.

--- src/main/java/es/urjc/dad/poshart/controller/MyController.java
+++ src/main/java/es/urjc/dad/poshart/controller/MyController.java
@@ -3,10 +3,15 @@ package es.urjc.dad.poshart.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import es.urjc.dad.poshart.model.Image;
+import es.urjc.dad.poshart.model.User;
 import es.urjc.dad.poshart.repository.UserRepository;
 import es.urjc.dad.poshart.service.ImageService;
 
@@ -21,19 +27,43 @@ import es.urjc.dad.poshart.service.ImageService;
 public class MyController {
 	
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 	
 	@Autowired
 	private ImageService imageService;
 	
-	@GetMapping("/")
-	public String startPage(Model model) {
-		return "start";
+	@PostConstruct
+	public void init() {
+		User u = new User();
+		for(int i = 0; i<20; i++){
+			u = new User("Correo "+i, "Usuario "+i, "Contraseña "+i);
+			userRepository.save(u);
+		}
 	}
 	
-	@GetMapping("/mustache")
-	public String devuelvePlantilla() {
+	@GetMapping("/")
+	public String startPage(Model model) {
 		return "MiPlantilla";
+	}
+	@GetMapping("/muro")
+	public String getMuro(Model model) {
+		return "muro";
+	}
+	@GetMapping("/shopping")
+	public String getShopping(Model model) {
+		return "shoppingCart";
+	}
+	@GetMapping("/user/{id}")
+	public String getUser(Model model, @PathVariable long id) {
+		User u = userRepository.findById(id).orElseThrow();
+		model.addAttribute("user", u);
+		return "users";
+	}
+	@GetMapping("/users")
+	public String getUser(Model model,Pageable page) {
+		Page<User> p = userRepository.findAll(page);
+		model.addAttribute("page", p);
+		return "allusers";
 	}
 	
 	@PostMapping("/newImage")
--- src/main/java/es/urjc/dad/poshart/repository/UserRepository.java
+++ src/main/java/es/urjc/dad/poshart/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package es.urjc.dad.poshart.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.urjc.dad.poshart.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Page<User> findAll(Pageable page);
+	
 }
