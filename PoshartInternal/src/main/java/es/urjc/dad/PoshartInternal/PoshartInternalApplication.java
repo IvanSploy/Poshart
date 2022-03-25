076041cca11b30f38444a89ff827b9ce2c6349ad
evitafumar <evapastorvideojuegos@gmail.com>
2022-03-25 12:35:29 +0100

    Creating Internal Service
    Se crea el servicio interno
    
    Hemos preparado el github para tener la aplicación principal y el servicio interno

---
+++ PoshartInternal/src/main/java/es/urjc/dad/PoshartInternal/PoshartInternalApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.dad.PoshartInternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshartInternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoshartInternalApplication.class, args);
+	}
+
+}
