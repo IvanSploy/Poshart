d3a27fab192c6b19d2a489d22fef5b5b666432ae
GuilleIngVid <78254966+GuilleIngVid@users.noreply.github.com>
2022-05-01 21:06:26 +0200

    Sesión distribuida con Hazelcast
    Sesión se mantiene en el balanceador
    
    Con el fin de que la sesión con la aplicación se mantenga, se hace uso de hazelcast.

--- Poshart/src/main/java/es/urjc/dad/poshart/PoshartApplication.java
+++ Poshart/src/main/java/es/urjc/dad/poshart/PoshartApplication.java
@@ -1,18 +1,23 @@
 package es.urjc.dad.poshart;
 
 import java.security.SecureRandom;
+import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class PoshartApplication {
 
 	@Bean
@@ -23,6 +28,16 @@ public class PoshartApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		joinConfig.getMulticastConfig().setEnabled(true);
+		
+		return config;
 	}
 	
 	public static void main(String[] args) {
